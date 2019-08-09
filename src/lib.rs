9e06ab965d3e9edb791e75b21ccdd5e99312fdde
yvt <i@yvt.jp>
2019-08-09 21:57:41 +0900

    Update doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,15 @@
 //! This crate provides a software-rendered surface for `winit`.
 //!
-//! The goal of this crate is to provide the minimal drawing functionality
+//! The goal of this crate is to provide a minimal drawing functionality
 //! for every platform supported by `winit` even if the drawing APIs that we
-//! usually assume are available, such as OpenGL ¹, aren't available on the
+//! usually assume are available, such as OpenGL¹, aren't available in the
 //! target environment. This crate is also useful as a fallback when they are
 //! available, but failed due to an unrecoverable error.
 //!
 //! ¹ [“Servo on Windows in VirtualBox gets 'NoAvailablePixelFormat'” servo/servo #9468](https://github.com/servo/servo/issues/9468)
 //!
 //! To this end, this crate is designed to panic only when preconditions are not
-//! met or under very specical circumstances.
+//! met or under very pathologic circumstances that would cause winit to panic.
 //!
 //! # Unimplemented features
 //!
